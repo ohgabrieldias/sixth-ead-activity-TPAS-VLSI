456d78563e60bf2b113f9791e4947a3e0184191c
Gabriel R. Dias <gabrielrd.aluno@unipampa.edu.br>
2023-12-04 19:06:18 -0300

    corrigindo identação

--- game_leds_x_buttons.v
+++ game_leds_x_buttons.v
@@ -2,8 +2,8 @@
 // # Copyright (c) [2023] [Gabriel Rosa Dias][Pedro Antonio]
 // # See LICENSE file for full license text.
 module game_leds_x_buttons (
-    input clk,           // Sinal de clock
-    input rst,           // Sinal de reset
+    input osc_clk,           // Sinal de clock
+    input reset_n,           // Sinal de reset
     input [3:0] btn,      // Botões (um para cada LED)
     output  [3:0] led    // LEDs (um para cada botão)
 );
@@ -23,25 +23,29 @@ reg [3:0] round_count;      // Conta as rodadas
 reg [3:0] correct_count;    // Conta os acertos consecutivos
 
 // Lógica do jogo
-always @(posedge clk or posedge rst) begin
-    if (rst) begin
-       pattern <= 3'b0;
-       user_input <= 3'b0;
-       pattern_pos <= 2'b0;
-       correct_pos <= 2'b0;
-       rnd_sel <= 2'b0;
-    end else begin
-        // Lógica principal do jogo
-        if (game_active) begin
-            // Geração do padrão na primeira rodada
-            if (round_count == 0) begin
-                rnd_sel <= $random % 4;
-                pattern <= 4'b0;
-                pattern[rnd_sel] <= 1;
-                pattern_pos <= 0;
-            end // if
-        end // if2
-    end // else
-end // always
-
+always @(posedge osc_clk or posedge reset_n)
+    begin
+        if (reset_n)
+            begin
+                pattern <= 3'b0;
+                user_input <= 3'b0;
+                pattern_pos <= 2'b0;
+                correct_pos <= 2'b0;
+                rnd_sel <= 2'b0;
+            end
+        else 
+            begin
+            // Lógica principal do jogo
+            if (game_active)
+                begin
+                    // Geração do padrão na primeira rodada
+                    if (round_count == 0) begin
+                        rnd_sel <= $random % 4;
+                        pattern <= 4'b0;
+                        pattern[rnd_sel] <= 1;
+                        pattern_pos <= 0;
+                    end // if
+                end // if2
+        end // else
+    end // always
 endmodule
