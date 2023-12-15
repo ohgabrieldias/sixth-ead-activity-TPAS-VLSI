57bab88bf4c51b8db493eff61f7802c33e635552
pedroanto <95453438+pedroanto@users.noreply.github.com>
2023-12-15 18:22:56 -0300

    Update game_leds_x_buttons.v

--- game_leds_x_buttons.v
+++ game_leds_x_buttons.v
@@ -1,77 +1,104 @@
 // # MIT License
-// # Copyright (c) [2023] [Gabriel Rosa Dias] [Pedro Antônio]
+// # Copyright (c) [2023] [Gabriel Rosa Dias] [Pedro Antonio]
 // # See LICENSE file for full license text.
 
 
-module game_leds_x_buttons (
+module game_leds_x_buttons #(parameter WIDTH = 3)(
     input osc_clk,           // Sinal de clock
     input reset_n,           // Sinal de reset
-    input [3:0] button,      // Botões (um para cada LED)
-    output reg [3:0] led    // LEDs (um para cada botão)
+    input [WIDTH:0] button,      // Botões€ (um para cada LED)
+    output reg [WIDTH:0] led    // LEDs (um para cada botão€)
 );
 
-integer i;
+integer i = 0;
 
 reg [15:0] padrao;
-reg [3:0] entrada_usuario[15:0]; // Entrada do usuário
-reg [3:0] qtd_digitos_corretos;  // Quantidade de dígitos corretos
+reg [15:0] entrada_usuario; // Entrada do usuário
+reg [3:0] qtd_digitos_corretos;  // Quantidade de digitos corretos
 reg [3:0] subconjunto;
-reg [2:0] rnd_sel;          // Seleção aleatória de LEDs
+reg [3:0] tmp;
+reg [2:0] rnd_sel;          // Seleção de LEDs
 reg [2:0] count_round;      // Contador de rodadas
 
-// Clock com uma frequência de 1/50 MHz=T  T=20×10 -9s
-// Quantidade de ciclos para 1s: 1/20×10 -9s=50×10 6 ciclos
-reg [25:0] count;           // Contador de tempo para contar até 50000000
+
+// Quantidade de ciclos para 1s: 1/20 6 ciclos
+reg [25:0] count;           // Contador de tempo para contar a 50000000
 reg [15:0] lfsr;            // Linear Feedback Shift Register for random number generation
+//wire resetBSN = 1'b1;
 
+wire resetInv;
+assign resetInv = ~reset_n;
 
-always @(posedge osc_clk or posedge reset_n)
+always @(posedge osc_clk)
     begin
-        if (reset_n)
+        if (resetInv)
             begin
-                padrao <= 4'b0;
-                subconjunto <= 4'b0;
+                padrao <= 16'b1;
+                subconjunto <= 4'b1111;
                 count_round <= 3'b0;
-                count <= 25'd0;
-                lfsr <= 16'hACE1; // Initial value for LFSR
+                count <= 26'd0;
+                lfsr <= 16'b0101010101010101; // Initial value for LFSR
+                led <= 4'b1111;     // LEDs apagados
+                i <= 0;
             end
         else
-            if (count_round == 3'b0)        // Round 0
+            if (count_round == 3'b0) 
                 begin
-                    for (i = 0; i < 4; i = i + 1)
+                    if (count == 50000000 && i < 4)      // round 0 (gera padrão e mostra na placa, um padrão a cada 1 s)
                         begin
-                            rnd_sel = lfsr[1:0]; // Select a random LED based on 2 LSBs of LFSR
-                            subconjunto[rnd_sel] = 1; // Seleciona um LED aleatoriamente
-                            padrao[i*4 +: 4] = subconjunto; // Atualiza a matriz de saída com o subconjunto
-                            subconjunto = 4'b0; // Reset subset for the next iteration
-                            lfsr = lfsr ^ (lfsr << 1) ^ (lfsr << 2) ^ (lfsr << 3); // Update LFSR
-                        end
-                    count_round <= count_round + 1;     // Go to next round
-                end
-            else                            // Round 1, 2, 3
-                begin
-                    if (count == 25'd50000000 && count_round == 1)      // 1s
-                        begin
-                            for (i = 0; i < 4; i = i + 1)   // Update LEDs based on previous round
-                                begin
-                                    subconjunto = padrao[i*4 +: 4]; // Get subset from previous round
-                                    led = subconjunto; // Atualiza a matriz de saída com o subconjunto
-                                    subconjunto = 4'b0; // Reset subset for the next iteration
-                                end
-                            count_round <= count_round + 1;     // Go to next round
+                            lfsr <= (lfsr ^ (lfsr << 5) ^ (lfsr >> 10))*13;
+                            if(lfsr < 22000) begin
+                                subconjunto <= 4'b1110;
+                            end 
+                            else if(lfsr > 22000 && lfsr < 34000)begin
+                                subconjunto <= 4'b1101;
+                            end
+                            else if(lfsr > 34000 && lfsr < 45000) begin
+                                subconjunto <= 4'b1011;
+                            end
+                            else begin
+                                subconjunto <= 4'b0111;
+                            end
+
+                            // led = subconjunto;    // testar
+                            padrao[i*4 +: 4] = subconjunto; // Atualiza a matriz de saÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â­da com o subconjunto
+                            tmp =  padrao[i*4 +: 4];
+                            led = tmp;
                             count <= 26'd0;     // Reset counter for 1s
+                            i = i + 1;
                         end
+                    else
+                        count <= count + 1;
+                        if (i == 4)
+                            begin
+                                count_round <= 3'b1;
+                                count <= 26'd0;     // Reset counter for 1s
+                            end
                 end
-                if (count_round == 3'b1)   // Round 1, Usuário escolhe LEDs
-                   begin
-                    if (button[0] || button[1] || button[2] || button[3]) begin
-                        entrada_usuario[count_round - 1] = button;
-                    end
-                    if (count == 25'd50000000) begin
-                        count_round <= count_round + 1;    // Go to next round
-                        count <= 26'd0;     // Reset counter for 1s
-                    end
-               count <= count + 1;     // Increment counter for 1s 
-             end
+            else if (count_round == 3'b1)
+             begin
+             led <= 4'b0000;
+             i = 0;
+        if (count == 50000000 && i < 4)  // round 1 (usuário tenta repetir o padrão, um padrão a cada 1 s)
+            begin
+                count <= 26'd0;  // Reset counter for 1s
+                entrada_usuario[i*4 +: 4] <= button;
+                // Blink the LED associated with the pressed button
+                led[button] <= ~led[button];
+
+                if (entrada_usuario[i*4 +: 4] == padrao[i*4 +: 4]) begin
+                    qtd_digitos_corretos <= qtd_digitos_corretos + 1;
+                end
+
+                i = i + 1;
+            end
+        else
+            begin
+                count <= count + 1;
+                count_round <= 3'b10;
+                i = 0;
+            end
       end
+
+end
 endmodule
